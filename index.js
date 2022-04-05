e81cc1e1526c8b482c2e2fd219f5adf5c8efff49
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2022-04-05 13:47:34 -0300

    add javascript tests

---
+++ index.js
@@ -0,0 +1,11 @@
+const teste = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const area = document.getElementById("area").value;
+
+    console.log({
+        name,
+        email,
+        area
+    });
+};
